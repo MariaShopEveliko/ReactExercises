517606b00889841e35d979ec3a124cd36ec5d84c
Maria Filipova <maria.shop@eveliko.com>
2023-12-11 11:16:00 +0200

    Style course

--- react-art-css/src/components/AuthInputs/AuthInputs.jsx
+++ react-art-css/src/components/AuthInputs/AuthInputs.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { styled } from 'styled-components';
 import "./AuthInputs.css";
+import Button from '../Button';
+import CustomInput from '../Input';
 
 const ControlWrp = styled.div`
   display: flex;
@@ -8,28 +10,6 @@ const ControlWrp = styled.div`
   gap: 0.5rem;
   margin-bottom: 1.5rem;
 `; //Tagged template
-
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: #6b7280;
-  color: ${({invalidlabel}) => invalidlabel ? "#f87171" : "#6b7280"}
-`;
-
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: #d1d5db;
-  color: #374151;
-  border: 1px solid transparent;
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -54,30 +34,14 @@ export default function AuthInputs() {
   return (
     <div id="auth-inputs">
       <ControlWrp>
-        <p>
-          <Label className={`label ${emailNotValid ? 'invalid' : undefined}`}>Email</Label>
-          <Input
-            type="email"
-            className={emailNotValid ? 'invalid' : undefined}
-            onChange={(event) => handleInputChange('email', event.target.value)}
-          />
-        </p>
-        <p>
-          <Label invalidlabel={passwordNotValid}>Password</Label>
-          <Input
-            type="password"
-            style={{ backgroundColor: emailNotValid ? "#fed2d2" : "#d1d5db" }}
-            onChange={(event) =>
-              handleInputChange('password', event.target.value)
-            }
-          />
-        </p>
+        <CustomInput label="Email" invalid={emailNotValid} type="email" onChange={(event) => handleInputChange('email', event.target.value)} />
+        <CustomInput label="Password" invalid={passwordNotValid} type="password" onChange={(event) => handleInputChange('password', event.target.value)} />
       </ControlWrp>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
