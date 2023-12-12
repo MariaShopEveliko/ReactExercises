18e376ffcad937c9204729ce9d280cee2ae559e5
Maria Filipova <maria.shop@eveliko.com>
2023-12-12 13:45:38 +0200

    New Project form

--- project-management/src/App.jsx
+++ project-management/src/App.jsx
@@ -20,15 +20,35 @@ function App() {
     });
   }
 
-  if (projectsState.selectedProjectId === null) {
-    content = <NewProject />;
-  } else if (projectsState.selectedProjectId === undefined) {
-    content =<NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const newProject = { ...projectData, id: Math.random() };
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...projectsState.projects, newProject],
+      };
+    });
   }
 
+  function handleCancelAddProject(){
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
+  }
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+  
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
       {content}
     </main>
   );
--- project-management/src/components/NewProject.jsx
+++ project-management/src/components/NewProject.jsx
@@ -1,21 +1,62 @@
+import { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 
-export default function NewProject() {
+export default function NewProject({ onAdd, onCancel }) {
+  const modal = useRef();
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    });
+  }
+
   return (
-    <div className="w-[35rem] mt-16"> {/* w-[35rem] - this way we can add custom classes */}
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">Cancel</button>
-        </li>
-        <li>
-          <button className="bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md">Save</button>
-        </li>
-      </menu>
-      <div>
-        <Input label="Title"/>
-        <Input label="Description" isTextarea/> {/* same as isTextarea={true} */}
-        <Input label="Due Date"/>
+    <>
+      <Modal ref={modal} buttonCaption="Close">
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalid input</h2>
+        <p className="text-stone-600 mb-4">Invalid value is entered</p>
+      </Modal>
+      <div className="w-[35rem] mt-16"> {/* w-[35rem] - this way we can add custom classes */}
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950" onClick={onCancel}>
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              className="bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={title} label="Title" />
+          <Input ref={description} label="Description" isTextarea />{/* same as isTextarea={true} */}
+          <Input type="date" ref={dueDate} label="Due Date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
