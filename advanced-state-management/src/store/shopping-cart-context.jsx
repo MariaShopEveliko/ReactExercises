f96169879880cf66427d5c4160dfff9048bf39db
Maria Filipova <maria.shop@eveliko.com>
2023-12-13 14:22:15 +0200

    Implement Context variables

--- advanced-state-management/src/store/shopping-cart-context.jsx
+++ advanced-state-management/src/store/shopping-cart-context.jsx
@@ -2,6 +2,6 @@ import { createContext } from "react";
 
 export const CartContext = createContext({
     items: [], //it's good to add a default value
-    addItemToCart: () => {}
+    addItemToCart: () => {},
+    updatedItemQuantity: () => {}
 });
-
